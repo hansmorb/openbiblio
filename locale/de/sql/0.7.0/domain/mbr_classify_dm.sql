755cbe945bc0b97a3671b4b8132636653bffc1ed
HansMorb <6433480+hansmorb@users.noreply.github.com>
2022-08-27 17:29:52 +0200

    moved src to different directory

--- locale/de/sql/0.7.0/domain/mbr_classify_dm.sql
+++
@@ -1,2 +0,0 @@
-insert into %prfx%mbr_classify_dm values ('1','Erwachsene','Y',0.00);
-insert into %prfx%mbr_classify_dm values ('2','Jugendliche','N',0.00);
--- locale/de/sql/0.7.1/domain/transaction_type_dm.sql
+++
@@ -1,3 +0,0 @@
-insert into %prfx%transaction_type_dm values ('-p','Zahlung','Y');
-insert into %prfx%transaction_type_dm values ('-r','Gutschrift','N');
-insert into %prfx%transaction_type_dm values ('+c','Gebühr','N');
