a401d485a1ceb785e2e903bc155f896b3678580c
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-09-18 15:44:54 -0400

    redid const syntax

--- src/shared/global_constants.php
+++ src/shared/global_constants.php
@@ -10,9 +10,9 @@
  * OBIB_BOTH - both assoc and numeric array result type
  ****************************************************************************
  */
-define("OBIB_ASSOC","1");
-define("OBIB_NUM","2");
-define("OBIB_BOTH","3");
+const OBIB_ASSOC = "1";
+const OBIB_NUM   = "2";
+const OBIB_BOTH  = "3";
 
 /****************************************************************************
  * search types:
@@ -21,43 +21,43 @@ define("OBIB_BOTH","3");
  * OBIB_SEARCH_SUBJECT
  ****************************************************************************
  */
-define("OBIB_SEARCH_BARCODE","1");
-define("OBIB_SEARCH_TITLE","2");
-define("OBIB_SEARCH_AUTHOR","3");
-define("OBIB_SEARCH_SUBJECT","4");
-define("OBIB_SEARCH_NAME","5");
-define("OBIB_SEARCH_CALLNO","6");
-define("OBIB_SEARCH_KEYWORD","7");
+const OBIB_SEARCH_BARCODE = "1";
+const OBIB_SEARCH_TITLE   = "2";
+const OBIB_SEARCH_AUTHOR  = "3";
+const OBIB_SEARCH_SUBJECT = "4";
+const OBIB_SEARCH_NAME    = "5";
+const OBIB_SEARCH_CALLNO  = "6";
+const OBIB_SEARCH_KEYWORD = "7";
 
 /****************************************************************************
  *  Misc. system constants
  ****************************************************************************
  */
-define("OBIB_CODE_VERSION","0.7.3g");
-define("OBIB_LATEST_DB_VERSION","0.7.1");
-define("OBIB_DEFAULT_STATUS","in");
-define("OBIB_STATUS_IN","in");
-define("OBIB_STATUS_OUT","out");
-define("OBIB_STATUS_ON_LOAN","ln");
-define("OBIB_STATUS_ON_ORDER","ord");
-define("OBIB_STATUS_SHELVING_CART","crt");
-define("OBIB_STATUS_ON_HOLD","hld");
-define("OBIB_MBR_CLASSIFICATION_JUVENILE","j");
-define("OBIB_DEMO_FLG",false);
-define("OBIB_HIGHLIGHT_I18N_FLG",false);
-define("OBIB_SEARCH_MAXPAGES",1000);
+const OBIB_CODE_VERSION = "0.7.3g";
+const OBIB_LATEST_DB_VERSION = "0.7.1";
+const OBIB_DEFAULT_STATUS = "in";
+const OBIB_STATUS_IN = "in";
+const OBIB_STATUS_OUT = "out";
+const BIB_STATUS_ON_LOAN = "ln";
+const BIB_STATUS_ON_ORDER = "ord";
+const OBIB_STATUS_SHELVING_CART = "crt";
+const OBIB_STATUS_ON_HOLD = "hld";
+const OBIB_MBR_CLASSIFICATION_JUVENILE = "j";
+const OBIB_DEMO_FLG = FALSE;
+const OBIB_HIGHLIGHT_I18N_FLG = FALSE;
+const OBIB_SEARCH_MAXPAGES = 1000;
 
-define("OBIB_MYSQL_DATETIME_TYPE","datetime");
-define("OBIB_MYSQL_DATETIME_FORMAT","Y-m-d H:i:s");
-define("OBIB_MYSQL_DATE_TYPE","date");
-define("OBIB_MYSQL_DATE_FORMAT","Y-m-d");
+const OBIB_MYSQL_DATETIME_TYPE = "datetime";
+const OBIB_MYSQL_DATETIME_FORMAT = "Y-m-d H:i:s";
+const OBIB_MYSQL_DATE_TYPE = "date";
+const OBIB_MYSQL_DATE_FORMAT = "Y-m-d";
 
-define("OBIB_BARCODE_RE", '/^[A-Za-z0-9._\/\-]+$/');
+const OBIB_BARCODE_RE = '/^[A-Za-z0-9._\/\-]+$/';
 
-define("OBIB_LOCALE_ROOT","../locale");
+const OBIB_LOCALE_ROOT = "../locale";
 
 # Not fully implemented yet.
-define("DB_TABLENAME_PREFIX", "");
+const DB_TABLENAME_PREFIX = "";
 const BARCODENMBR = "barcodeNmbr";
 
 ?>
