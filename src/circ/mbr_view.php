10a51de227bb28e2e74cbadc52a39e12a929d0f3
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-09-18 16:17:54 -0400

    fixed some array keys not necessarily defined

--- src/circ/mbr_view.php
+++ src/circ/mbr_view.php
@@ -80,8 +80,8 @@
   #*  Derzeit abgedeckt durch Missbrauch des Fehlercodes und Markierung mit !!! davor
   #****************************************************************************
   $dueMsg = "";
-  $pgErrors = $_SESSION['pageErrors'];
-  if (str_starts_with($pgErrors[\BARCODENMBR], '!!!')) {
+  $pgErrors = $_SESSION['pageErrors'] ?? '';
+  if ($pgErrors && str_starts_with($pgErrors[\BARCODENMBR], '!!!')) {
 	  $dueMsg = "<font class=\"error\">".substr($pgErrors[\BARCODENMBR], 3)."</font><br><br>";
 	  unset($postVars);
   	  unset($pageErrors);
